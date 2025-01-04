a1709f53b056edf320251d510feb1d5d93c516fe
Aaron Glover <aglove2189@gmail.com>
2025-01-04 17:13:39 -0600

    init

---
+++ src/app.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+import july
+import holidays
+import streamlit as st
+
+from optimize import create_days, maximize_pto
+
+
+if __name__ == "__main__":
+    st.set_page_config("Max PTO", page_icon="🌴")
+    st.title("🌴 Max PTO")
+    cols = st.columns(2)
+    current_year = datetime.now().year
+    years = list(range(current_year - 10, current_year + 11))
+    year = cols[0].selectbox("Select Year", years, index=len(years) // 2)
+
+    country_holidays = holidays.country_holidays("US", years=year)
+
+    pto_hours = cols[1].number_input("PTO Hours", value=160, min_value=1, max_value=52*40)
+
+    with st.expander("Work Schedule"):
+        monday = st.number_input("Monday", value=8.75, min_value=0., max_value=24., step=1.)
+        tuesday = st.number_input("Tuesday", value=8.75, min_value=0., max_value=24., step=1.)
+        wednesday = st.number_input("Wednesday", value=8.75, min_value=0., max_value=24., step=1.)
+        thursday = st.number_input("Thursday", value=8.75, min_value=0., max_value=24., step=1.)
+        friday = st.number_input("Friday", value=5., min_value=0., max_value=24., step=1.)
+        saturday = st.number_input("Saturday", value=0., min_value=0., max_value=24., step=1.)
+        sunday = st.number_input("Sunday", value=0., min_value=0., max_value=24., step=1.)
+        work_hours = [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
+
+    days = create_days(year, country_holidays, work_hours)
+    optimal_pto_days = sorted(maximize_pto(days, pto_hours, country_holidays))
+
+    data = []
+    for i in days:
+        if i.date in optimal_pto_days:
+            value = 2
+        elif i.date in country_holidays:
+            value = 1
+        else:
+            value = 0
+        data.append(value)
+
+    calendar_plot = july.calendar_plot(
+        [i.date for i in days], data, weeknum_label=False, date_label=True
+    )
+    st.pyplot(calendar_plot[0][0].get_figure())
+
+    pto_taken = sum([i.hours for i in days if i.is_pto])
+    st.info(f"PTO Taken: {pto_taken}")
+
+    st.dataframe({"PTO Dates": optimal_pto_days})
---
+++ src/optimize.py
@@ -0,0 +1,54 @@
+from datetime import date, timedelta
+
+
+class Day:
+    def __init__(self, date, work_hours, country_holidays):
+        self.date = date
+        self.is_weekday = date.weekday() < 5
+        self.is_holiday = date in country_holidays
+        self.is_workday = self.is_weekday and not self.is_holiday
+        self.hours = work_hours[date.weekday()] if self.is_workday else 0
+        self.is_pto = False
+
+
+def maximize_pto(days, total_pto_hours, holidays):
+    remaining_pto = total_pto_hours
+    pto_days = []
+
+    sorted_days = sorted(
+        [day for day in days if day.is_workday],
+        key=lambda d: (
+            d.date - timedelta(days=1) in holidays or d.date + timedelta(days=1) in holidays,  # Next to a holiday
+            d.date.weekday() == 4,  # Fridays (for long weekends)
+            d.hours  # Lower-hour workdays first
+        ),
+        reverse=True  # Higher priority first
+    )
+
+    for i, day in enumerate(sorted_days):
+        if remaining_pto <= 0:
+            break
+
+        if not day.is_pto and remaining_pto >= day.hours:
+            if i + 1 < len(days) and i - 1 >= 0:
+                next_day = days[i + 1]
+                prev_day = days[i - 1]
+
+                if not prev_day.is_workday or not next_day.is_workday:
+                    day.is_pto = True
+                    pto_days.append(day.date)
+                    remaining_pto -= day.hours
+
+    return pto_days
+
+
+def create_days(year, holidays, work_hours):
+    start_dt = date(year, 1, 1)
+    end_dt = date(year + 1, 1, 1)
+
+    days = []
+    for i in range((end_dt - start_dt).days):
+        dt = start_dt + timedelta(days=i)
+        days.append(Day(dt, work_hours, holidays))
+
+    return days
