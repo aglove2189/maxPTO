db3d51f6aabd1f7b98a74af568aa39a89640df7d
Aaron Glover <aglove2189@gmail.com>
2025-01-04 18:52:03 -0600

    edit holidays

--- src/app.py
+++ src/app.py
@@ -10,13 +10,11 @@ from optimize import create_days, maximize_pto
 if __name__ == "__main__":
     st.set_page_config("Max PTO", page_icon="🌴")
     st.title("🌴 Max PTO")
+
     cols = st.columns(2)
     current_year = datetime.now().year
     years = list(range(current_year - 10, current_year + 11))
     year = cols[0].selectbox("Select Year", years, index=len(years) // 2)
-
-    country_holidays = holidays.country_holidays("US", years=year)
-
     pto_hours = cols[1].number_input("PTO Hours", value=160, min_value=1, max_value=52*40)
 
     with st.expander("Work Schedule"):
@@ -29,14 +27,18 @@ if __name__ == "__main__":
         sunday = st.number_input("Sunday", value=0., min_value=0., max_value=24., step=1.)
         work_hours = [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
 
-    days = create_days(year, country_holidays, work_hours)
-    optimal_pto_days = sorted(maximize_pto(days, pto_hours, country_holidays))
+    with st.expander("Holiday Schedule"):
+        country_holidays = holidays.country_holidays("US", years=year)
+        country_holidays_edited = st.data_editor(country_holidays, num_rows="dynamic")
+
+    days = create_days(year, country_holidays_edited, work_hours)
+    optimal_pto_days = sorted(maximize_pto(days, pto_hours, country_holidays_edited))
 
     data = []
     for i in days:
         if i.date in optimal_pto_days:
             value = 2
-        elif i.date in country_holidays:
+        elif i.date in country_holidays_edited:
             value = 1
         else:
             value = 0
